29e4946dedf5559a295f1566db876555260cb5dc
Mathonwy Thomas <mathonwy.thomas@arrival.com>
2023-01-11 13:28:01 +0000

    Add can_hold function to Rectangle impl block

--- area_calculator/src/main.rs
+++ area_calculator/src/main.rs
@@ -4,6 +4,16 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other_rect: &Self) -> bool {
+        self.width > other_rect.width && self.height > other_rect.height
+    }
+}
+
 fn main() {
     let width1 = 30;
     let height1 = 50;
@@ -27,7 +37,25 @@ fn main() {
         "The area of the rect from a struct is {} square pixels.",
         area_struct(&rect2)
     );
+    println!(
+        "The area of the rect from a struct method is {} square pixels.",
+        rect2.area()
+    );
     println!("Rect is {:#?}", rect2);
+
+    let big_rect = Rectangle {
+        width: 35,
+        height: 45,
+    };
+    let small_rect = Rectangle {
+        width: 30,
+        height: 10,
+    };
+    println!(
+        "Big rect can hold small rect: {}",
+        big_rect.can_hold(&small_rect)
+    );
+    println!("Big rect can hold rect2: {}", big_rect.can_hold(&rect2));
 }
 
 fn area_individual(width: u32, height: u32) -> u32 {
